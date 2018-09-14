acf0b23099bccb2f7502cc05eeab4e000dcd5fa0
Ящук Александр <jashhuk@vistgroup.ru>
2018-09-14 11:29:07 +0300

    В Fabric для деплоя: Отправка архива приложения на удалённый сервер

--- fabfile.py
+++ fabfile.py
@@ -1,39 +1,52 @@
 # -*- coding: UTF-8 -*-
 
 import os
-import getpass
 
 from fabric.api import task
-from fabric.operations import run, local, prompt, put
 from fabric.state import env
+from fabric.operations import run, local, prompt, put
 
-env.hosts = 'r5am.ru'
-env.port = '35712'
-env.user = "zoer" 
-# env.password = "123"
+from fabconf import REMOTE_HOST, REMOTE_PORT, REMOTE_USER
+
+env.hosts = REMOTE_HOST
+env.port = REMOTE_PORT
+env.user = REMOTE_USER
 env.shell = "bash"
 home_dir = os.getenv('HOME')
-project_dir = home_dir + os.path.sep + "go" + os.path.sep + "src" + os.path.sep + "trudza40"
+project_name = "trudza40"
+archive_file_name = project_name + ".tar.gz"
+project_dir = home_dir + os.path.sep + "go" + os.path.sep + "src" + os.path.sep + project_name
 
 
 @task(default=True)
 def start():
+    """ Запускать всё по очереди """
     make_archive()
+    send_to_remote_server()
 
 
 @task()
 def make_archive():
-    """ 1.Собрать архив проекта через bee pack """
+    """ Собрать архив проекта через 'bee pack' """
     print("\n ==> Make archive ...")
-    
-    local("cd " + project_dir + "; " + 
-            home_dir + os.path.sep + "go/bin/bee pack;" + 
-            "echo; " + 
-            "ls -la | grep 'tar.gz'"
-    )
-    
 
-# 2.Отправить архив на дальний сервер
+    local("cd " + project_dir + "; " +
+          home_dir + os.path.sep + "go/bin/bee pack;" +
+          "echo; " +
+          "ls -la | grep '{0}'".format(archive_file_name)
+          )
+
+
+@task()
+def send_to_remote_server():
+    """ Отправить архив на дальний сервер """
+    print("\n ==> Send archive to remote server ...")
+
+    remote_home_dir = home_dir
+    put(
+        project_dir + os.path.sep + archive_file_name,
+        remote_home_dir + os.path.sep + archive_file_name
+    )
 
 
 # 3.Забекапить старый проект
@@ -55,5 +68,3 @@ def make_archive():
 
 
 # 9.Послать месседж админу
-
-
